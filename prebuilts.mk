108f747c676dad894dcf86eaa83dd36930f9dba7
teamorchidos <71541333+teamorchidos@users.noreply.github.com>
2020-12-21 01:09:32 +0000

    neatened up

--- prebuilts.mk
+++ prebuilts.mk
@@ -2,21 +2,17 @@ include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_PACKAGES += \
     dnotes \
-    MagiskManager \
     Metro \
     taptap \ 
     recorder \
     material \
-    declutter \
     aod \
     mnml \
-    snapseed \
-    spike \
     smooz \
-    galleryo \
-    musie \ 
+    galleryo \ 
     walls \
     actionblocks \
     olauncher \
     calc \
     gtasks
+	twobird
