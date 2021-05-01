6fbe17b2bdca4156f42a95dde3c3b7b1d1b6ca2f
the orchidproject <aosporchid@gmail.com>
2021-05-01 16:48:10 +0100

    orchid os

--- prebuilts.mk
+++ prebuilts.mk
@@ -1,16 +1,13 @@
 include $(call first-makefiles-under,$(LOCAL_PATH))
 
 PRODUCT_PACKAGES += \
-    dnotes \
-    Metro \
-    taptap \
-    recorder \
-    material \
-    aod \
-    mnml \
-    walls \
-    gallerymod \
-    walls \
     calc \
-    AppWarden \
-    gsounds
+	gallerymod \
+	material \
+	Mixnotes \
+    mnml \
+    puretube \
+	recorder \
+	Soundspice \
+	tapap \
+	walls
